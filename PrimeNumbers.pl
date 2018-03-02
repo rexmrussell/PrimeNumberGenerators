d66f826065150a79d8736fd924590635838f2189
Brigham Russell <rexrussell1973@gmail.com>
2018-03-01 21:00:10 -0700

    Update PrimeNumbers.pl

--- PrimeNumbers.pl
+++ PrimeNumbers.pl
@@ -15,19 +15,19 @@ $pn=scalar(@pn); # get number of values in "pn"
 print $i."\n"; # print prime numbers to screen
 if($pn==$n){last;}}} # exit after "n" primes have been generated
 
-// Tested on March 1, 2018
-//
-// Script Data:
-// Generated 100,000 prime numbers starting with 2 and ending with 1,299,709
-//
-// TIME: real	0m5.748s
-// TIME: user	0m5.724s
-// TIME: sys	0m0.008s
-//
-// OS: Amazon Linux
-// vendor_id	: GenuineIntel
-// model name	: Intel(R) Xeon(R) CPU E5-2676 v3 @ 2.40GHz
-// cpu MHz		: 2394.562
-// cache size	: 30720 KB
-// MemTotal: 2.049 GB
-//
+# Tested on March 1, 2018
+#
+# Script Data:
+# Generated 100,000 prime numbers starting with 2 and ending with 1,299,709
+#
+# TIME: real	0m5.748s
+# TIME: user	0m5.724s
+# TIME: sys	0m0.008s
+#
+# OS: Amazon Linux
+# vendor_id	: GenuineIntel
+# model name	: Intel(R) Xeon(R) CPU E5-2676 v3 @ 2.40GHz
+# cpu MHz		: 2394.562
+# cache size	: 30720 KB
+# MemTotal: 2.049 GB
+#
