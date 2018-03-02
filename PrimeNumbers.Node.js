97edc5ac8a3d1bc1d1d8219a56880f4915944df8
Brigham Russell <rexrussell1973@gmail.com>
2018-03-01 20:30:20 -0700

    Create PrimeNumbers.Node.js

---
+++ PrimeNumbers.Node.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+var sqrt=require('math-sqrt'); // use the math squareroot module
+var n=100000; // check for n number of primes
+var PN=[2,3]; // the array into which the prime numbers will go
+console.log(PN[0]); // prints the first prime number
+console.log(PN[1]); // prints the second prime number
+var value; // holds the resuts of the modulus
+for(var i=3;i<=i+1;i=i+2){ // loops through odd numbers
+for(var ii=0;ii<sqrt(PN.length);ii++){ // loops through the "PN" array
+value=i%PN[ii]; // checks i with each value of "PN" for factorability
+if(value===0){break;}} // if i is not prime, then get the next i
+if(value!==0){PN.push(i); // if i is prime, then put it into "PN"
+console.log(i);} // print the results to terminal
+if(PN.length==n){break;}} // Exits once "n" primes have been generated
