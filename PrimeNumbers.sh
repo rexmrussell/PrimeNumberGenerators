373f14cdd92ec1c543746b25ebe22d00abd98043
Brigham Russell <rexrussell1973@gmail.com>
2018-03-01 21:04:05 -0700

    Update PrimeNumbers.sh

--- PrimeNumbers.sh
+++ PrimeNumbers.sh
@@ -19,3 +19,20 @@ then break # ... then quit the loop, which exits the program
 fi
 fi
 done;
+
+# Tested on March 1, 2018
+#
+# Script Data:
+# Generated 100 prime numbers starting with 2 and ending with 557
+#
+# TIME: real	0m3.798s
+# TIME: user	0m0.180s
+# TIME: sys	0m0.428s
+#
+# OS: Amazon Linux
+# vendor_id	: GenuineIntel
+# model name	: Intel(R) Xeon(R) CPU E5-2676 v3 @ 2.40GHz
+# cpu MHz		: 2394.562
+# cache size	: 30720 KB
+# MemTotal: 2.049 GB
+#
