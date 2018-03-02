8b659c3abc4b0fb3249ac482856edcddae2f7a0e
Brigham Russell <rexrussell1973@gmail.com>
2018-03-01 20:59:15 -0700

    Update PrimeNumbers.py

--- PrimeNumbers.py
+++ PrimeNumbers.py
@@ -12,19 +12,19 @@ for i in range(7,z,2): # loops through odd numbers to be check for prime
     if(x==n):break # exits program once "n" primes have been generated
 for z in PN:print z # print all primes to terminal
 
-// Tested on March 1, 2018
-//
-// Script Data:
-// Generated 100,000 prime numbers starting with 2 and ending with 1,299,709
-//
-// TIME: real	0m6.200s
-// TIME: user	0m6.124s
-// TIME: sys	0m0.052s
-//
-// OS: Amazon Linux
-// vendor_id	: GenuineIntel
-// model name	: Intel(R) Xeon(R) CPU E5-2676 v3 @ 2.40GHz
-// cpu MHz		: 2394.562
-// cache size	: 30720 KB
-// MemTotal: 2.049 GB
-//
+# Tested on March 1, 2018
+#
+# Script Data:
+# Generated 100,000 prime numbers starting with 2 and ending with 1,299,709
+#
+# TIME: real	0m6.200s
+# TIME: user	0m6.124s
+# TIME: sys	0m0.052s
+#
+# OS: Amazon Linux
+# vendor_id	: GenuineIntel
+# model name	: Intel(R) Xeon(R) CPU E5-2676 v3 @ 2.40GHz
+# cpu MHz		: 2394.562
+# cache size	: 30720 KB
+# MemTotal: 2.049 GB
+#
