d9a696f7437ee4ef9c3bd8e8abe05dfeb61c64a2
Brigham Russell <rexrussell1973@gmail.com>
2018-03-01 21:00:50 -0700

    Update PrimeNumbers.php

--- PrimeNumbers.php
+++ PrimeNumbers.php
@@ -17,18 +17,18 @@ print"$x\n"; # prints prime numbers to terminal
 if($np==$n){break;}}} # quits the program once there are "n" primes in "pn"
 ?>
 
-// Tested on March 1, 2018
-//
-// Script Data:
-// Generated 100,000 prime numbers starting with 2 and ending with 1,299,709
-//
-// TIME: real	0m2.076s
-// TIME: user	0m1.696s
-// TIME: sys	0m0.372s
-//
-// OS: Amazon Linux
-// vendor_id	: GenuineIntel
-// model name	: Intel(R) Xeon(R) CPU E5-2676 v3 @ 2.40GHz
-// cpu MHz		: 2394.562
-// cache size	: 30720 KB
-// MemTotal: 2.049 GB
+# Tested on March 1, 2018
+#
+# Script Data:
+# Generated 100,000 prime numbers starting with 2 and ending with 1,299,709
+#
+# TIME: real	0m2.076s
+# TIME: user	0m1.696s
+# TIME: sys	0m0.372s
+#
+# OS: Amazon Linux
+# vendor_id	: GenuineIntel
+# model name	: Intel(R) Xeon(R) CPU E5-2676 v3 @ 2.40GHz
+# cpu MHz		: 2394.562
+# cache size	: 30720 KB
+# MemTotal: 2.049 GB
