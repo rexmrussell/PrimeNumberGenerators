d0b2fe31279691a8c98b70ab9d2f17d1f8dcfac5
Brigham Russell <rexrussell1973@gmail.com>
2018-03-12 16:10:06 -0600

    Update PrimeNumbers.js

--- PrimeNumbers.js
+++ PrimeNumbers.js
@@ -9,4 +9,18 @@ if (v===0){break;}} // if i is not prime, then get the next i
 if (v!==0){PN.push(i); // if i is prime, then put it into "PN"
 console.log(i); // print the results to terminal
 if(PN.length===n){break;}}} // Exits once "n" primes have been generated
+
+// Tested on March 12, 2018
+//
+// Script Data:
+// Generated 100,000 prime numbers starting with 5 and ending with 1,299,709
+//
+// TIME: 51.782 Seconds
+//
+// MAC OS High Sierra Version 10.13.3
+// Processor 2.5 GHz Intel Core i7
+// Memory 16 GB 1600 MHz DDR3
+//
+// Firefox Quantum 58.0.2(64 bit)
+//
 </script>
