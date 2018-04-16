7db0d4ce59354ca845409c39b98adb8d6864a89f
Brigham Russell <rexrussell1973@gmail.com>
2018-04-16 12:49:03 -0600

    reformatted code

--- PrimeNumbers.php
+++ PrimeNumbers.php
@@ -5,16 +5,19 @@ $pn=array(2, 3); # the array into which the prime numbers will go
 print $pn[0]."\n"; # prints the first prime "2"
 print $pn[1]."\n"; # prints the second prime "3"
 $np=count($pn); # gets the number of values in the "pn" array
-for($x=3; $x<$x+1; $x=$x+2){ # loops through odd numbers to test for prime
-for($i=0; $i<sqrt($np);$i++){ # loops through the index values of the "pn" array
-$test=$x % $pn[$i]; # checks to see if any value "pn" will divide evenly into x
-if($test==0){ # if some value "pn" does divide evenly into x...
-break;}} # ... get the next value x
-if($test!=0){ # if no value "pn" divides evenly into x...
-$pn[]=$x; # add x to the "pn" array
-$np=count($pn); # get the number of values in the "pn" array
-print"$x\n"; # prints prime numbers to terminal
-if($np==$n){break;}}} # quits the program once there are "n" primes in "pn"
+for ($x=3; $x<$x+1; $x=$x+2) { # loops through odd numbers to test for prime
+  for ($i=0; $i<sqrt($np);$i++) { # loops through the index values of the "pn" array
+    $test=$x % $pn[$i]; # checks to see if any value "pn" will divide evenly into x
+    if ($test==0) { # if some value "pn" does divide evenly into x...
+      break;}  # ... get the next value x
+    }
+  if ($test!=0) { # if no value "pn" divides evenly into x...
+    $pn[]=$x; # add x to the "pn" array
+    $np=count($pn); # get the number of values in the "pn" array
+    print"$x\n"; # prints prime numbers to terminal
+    if ($np==$n) {break;} # quits the program once there are "n" primes in "pn"
+  }
+}
 ?>
 
 # Tested on March 1, 2018
