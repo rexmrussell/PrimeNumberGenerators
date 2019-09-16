d21a7954fa7d784014d8ef1b4f20543f50ac7292
Rex Russell <8qbits@gmail.com>
2019-09-15 19:47:53 -0600

    Update PrimeNumbers.sh

--- PrimeNumbers.sh
+++ PrimeNumbers.sh
@@ -4,8 +4,8 @@ o=n*100 # sets the program to create "n" number of primes
 declare pn=(2 3); # The array into which the primes will go
 for ((i=3;$i<$o;i=$i+2)) # Loop generates only odd numbers to test for prime
   do pnn=${#pn[@]}; # measures the size of the "pn" array
-  for (( ii=0; $ii<$pnn; ii=$ii+1)) # Loop creates index numbers for "pn" array
-    do prime=`expr $i % ${pn[$ii]}` # Modulus checks i with all values "pn"
+  for ((ii=0; $ii<$pnn; ii=$ii+1)) # Loop creates index numbers for "pn" array
+    do prime=$(expr $i % ${pn[$ii]}) # Modulus checks i with all values "pn"
     if (($prime==0)) # if a number from "pn" is able to divide into i...
       then break # ...then the next value i is tested
     fi
